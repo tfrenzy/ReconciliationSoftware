4445db6ce527a265f52dee0fe7d2fd0278428f97
tfrenzy <45123952+tfrenzy@users.noreply.github.com>
2021-04-18 12:58:11 +0100

    Add files via upload

---
+++ src/Reconciliation.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Tonye
+ */
+public class Reconciliation {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        ReconciliationForm form = new ReconciliationForm();
+        form.setVisible(true);
+    }
+
+}
